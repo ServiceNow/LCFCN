2331292f3738a2269b11e9bebe00ec0084d7bdcb
Issam H. Laradji <IssamLaradji@users.noreply.github.com>
2020-07-28 16:32:40 -0400

    better lcfcn loss

--- lcfcn/lcfcn_loss.py
+++ lcfcn/lcfcn_loss.py
@@ -10,141 +10,115 @@ from skimage.morphology import watershed
 from skimage.segmentation import find_boundaries
 
 
-def compute_lcfcn_loss(logits, points, reduction='mean',
-                       add_global_split=True, 
-                       roi_mask=None):
-    """Computes the lcfcn loss.
 
-    Parameters
-    ----------
-    logits : torch tensor of shape (n, c, h ,w)
-        Model output before the softmax
-    points : torch tensor of shape (n, h ,w)
-        Non-zero entries represent the locations and the class id of the objects
-
-    Returns
-    -------
-    loss: float
-        LCFCN loss
+def compute_loss(points, probs, roi_mask=None):
     """
-    n = logits.size(0)
-    assert n == 1
+    images: n x c x h x w
+    probs: h x w (0 or 1)
+    """
+    points = points.squeeze()
+    probs = probs.squeeze()
 
-    probs = F.softmax(logits, 1)
-    probs_log = F.log_softmax(logits, 1)
+    assert(points.max() <= 1)
 
-    # IMAGE LOSS
-    iloss = compute_image_loss(probs, points)
+    tgt_list = get_tgt_list(points, probs, roi_mask=roi_mask)
 
-    # POINT LOSS
-    ploss = F.nll_loss(probs_log, points,
-                       ignore_index=0,
-                       reduction='sum')
-
-    blob_dict = get_blob_dict(logits, points, roi_mask=roi_mask)
-
-    # split loss
-    sloss = compute_split_loss(probs_log, probs,
-                                   points, blob_dict, 
-                                   add_global_loss=add_global_split, 
-                                   reduction=reduction)
-
-    # FP loss
-    floss = compute_fp_loss(probs_log, blob_dict, 
-                                reduction=reduction)
-
-
-
-    return iloss + ploss + sloss + floss
-
-# Loss Utils
-
-
-def compute_image_loss(logits, points):
-    n, k, h, w = logits.size()
-
-    # get target
-    labels = torch.zeros(k, device=logits.device)
-    labels[points.unique()] = 1
-    logits_max = logits.view(n, k, h*w).max(2)[0].view(-1)
-
-    loss = F.binary_cross_entropy(logits_max, labels, reduction='sum')
-
-    return loss
-
-
-def compute_fp_loss(probs_log, blob_dict, reduction='sum'):
-    if blob_dict["n_fp"] == 0:
-        return 0.
-
-    blobs = blob_dict["blobs"]
-
+    # image level
+    # pt_flat = points.view(-1)
+    pr_flat = probs.view(-1)
     
+    # compute loss
     loss = 0.
-    n_fp = 0.
-    for b in blob_dict["blobList"]:
-        if b["n_points"] != 0:
-            continue
-
-        T = np.ones(blobs.shape[-2:])
-        T[blobs[b["class"]] == b["label"]] = 0
-
-        loss += F.nll_loss(probs_log, torch.LongTensor(T).to(device=probs_log.device)[None],
-                                   ignore_index=1, reduction='mean')
-        n_fp += 1
-
-    if reduction == 'mean':
-        loss = loss / max(n_fp, 1)
-
+    for tgt_dict in tgt_list:
+        pr_subset = pr_flat[tgt_dict['ind_list']]
+        # pr_subset = pr_subset.cpu()
+        loss += tgt_dict['scale'] * F.binary_cross_entropy(pr_subset, 
+                                        torch.ones(pr_subset.shape, device=pr_subset.device) * tgt_dict['label'], 
+                                        reduction='mean')
+    
     return loss
 
+@torch.no_grad()
+def get_tgt_list(points, probs, roi_mask=None):
+    tgt_list = []
 
-def compute_split_loss(probs_log, probs, points, blob_dict,
-                 add_global_loss=False, reduction='sum'):
-    if blob_dict["n_multi"] == 0:
-        return 0.
+    # image level
+    pt_flat = points.view(-1)
+    pr_flat = probs.view(-1)
 
-    blobs = blob_dict["blobs"]
-    probs_numpy = probs[0].detach().cpu().numpy()
-    points_numpy = points.cpu().numpy().squeeze()
+    u_list = points.unique()
+    if 0 in u_list:
+        ind_bg = pr_flat.argmin()
+        tgt_list += [{'scale': 1, 'ind_list':[ind_bg], 'label':0}]   
 
-    loss = 0.
-    n_multi = 0.
-    for b in blob_dict["blobList"]:
-        if b["n_points"] < 2:
-            continue
+    if 1 in u_list:
+        ind_fg = pr_flat.argmax()
+        tgt_list += [{'scale': 1, 'ind_list':[ind_fg], 'label':1}]   
 
-        l = b["class"] + 1
-        probs = probs_numpy[b["class"] + 1]
+    # point level
+    if 1 in u_list:
+        ind_fg = torch.where(pt_flat==1)[0]
+        tgt_list += [{'scale': len(ind_fg), 'ind_list':ind_fg, 'label':1}]  
 
-        points_class = (points_numpy == l).astype("int")
-        blob_ind = blobs[b["class"]] == b["label"]
+    # get blobs
+    probs_numpy = probs.detach().cpu().numpy()
+    blobs = get_blobs(probs_numpy, roi_mask=None)
 
-        T = watersplit(probs, points_class*blob_ind)*blob_ind
-        T = 1 - T
+    # get foreground and background blobs
+    points = points.cpu().numpy()
+    fg_uniques = np.unique(blobs * points)
+    bg_uniques = np.delete(np.unique(blobs), fg_uniques)
 
-        loss += (b["n_points"] - 1) * F.nll_loss(probs_log, torch.LongTensor(T).to(device=probs_log.device)[None],
-                                          ignore_index=1, reduction='mean')
-        n_multi += 1
-        
-    if reduction == 'mean':
-        loss = loss / max(n_multi, 1)
+    # split level
+    # -----------
+    n_total = points.sum()
 
-    if add_global_loss:
-        for l in range(1, probs_numpy.shape[1]):
-            points_class = (points_numpy == l).astype(int)
+    if n_total > 1:
+        # global split
+        boundaries = watersplit(probs_numpy, points)
+        ind_bg = np.where(boundaries.ravel())[0]
 
-            if points_class.sum() == 0:
+        tgt_list += [{'scale': (n_total-1), 'ind_list':ind_bg, 'label':0}]  
+
+        # local split
+        for u in fg_uniques:
+            if u == 0:
                 continue
 
-            T = watersplit(probs_numpy[l], points_class)
-            T = 1 - T
-            scale = float(points_class.sum())
-            # hu.save_image('tmp.png', T)
-            loss += scale * F.nll_loss(probs_log, torch.LongTensor(T).to(device=probs_log.device)[None],
-                               ignore_index=1, reduction='mean')
+            ind = blobs==u
 
-    return loss
+            b_points = points * ind
+            n_points = b_points.sum()
+            
+            if n_points < 2:
+                continue
+            
+            # local split
+            boundaries = watersplit(probs_numpy, b_points)*ind
+            ind_bg = np.where(boundaries.ravel())[0]
+
+            tgt_list += [{'scale': (n_points - 1), 'ind_list':ind_bg, 'label':0}]  
+
+    # fp level
+    for u in bg_uniques:
+        if u == 0:
+            continue
+        
+        b_mask = blobs==u
+        if roi_mask is not None:
+            b_mask = (roi_mask * b_mask)
+        if b_mask.sum() == 0:
+            pass
+            # from haven import haven_utils as hu
+            # hu.save_image('tmp.png', np.hstack([blobs==u, roi_mask]))
+            # print()
+        else:
+            ind_bg = np.where(b_mask.ravel())[0]
+            tgt_list += [{'scale': 1, 'ind_list':ind_bg, 'label':0}]  
+
+    return tgt_list 
+
+
 
 
 def watersplit(_probs, _points):
@@ -159,87 +133,18 @@ def watersplit(_probs, _points):
     return find_boundaries(seg)
 
 
-def get_blobs(logits, roi_mask=None):
-    n, k, _, _ = logits.shape
-    pred_mask = logits.max(1)[1].squeeze().cpu().numpy()
-    
-    h, w = pred_mask.shape
-    blobs = np.zeros((k - 1, h, w), int)
+def get_blobs(probs, roi_mask=None):
+    h, w = probs.shape
+ 
+    pred_mask = (probs>0.5).astype('uint8')
+    blobs = np.zeros((h, w), int)
 
-    for category_id in np.unique(pred_mask):
-        if category_id == 0:
-            continue
-        blobs[category_id - 1] = morph.label(pred_mask == category_id)
+    blobs = morph.label(pred_mask == 1)
 
     if roi_mask is not None:
         blobs = (blobs * roi_mask[None]).astype(int)
 
     return blobs
-
-
-@torch.no_grad()
-def get_blob_dict(logits, points, roi_mask=None):
-    blobs = get_blobs(logits, roi_mask=roi_mask)
-    points = points.cpu().numpy().squeeze()
-
-    if blobs.ndim == 2:
-        blobs = blobs[None]
-
-    blobList = []
-
-    n_multi = 0
-    n_single = 0
-    n_fp = 0
-    total_size = 0
-
-    for l in range(blobs.shape[0]):
-        class_blobs = blobs[l]
-        points_mask = points == (l+1)
-        # Intersecting
-        blob_uniques, blob_counts = np.unique(
-            class_blobs * (points_mask), return_counts=True)
-        uniques = np.delete(np.unique(class_blobs), blob_uniques)
-
-        for u in uniques:
-            blobList += [{"class": l, 
-                          "label": u, 
-                          "n_points": 0, 
-                          "size": 0,
-                          "pointsList": []}]
-            n_fp += 1
-
-        for i, u in enumerate(blob_uniques):
-            if u == 0:
-                continue
-
-            pointsList = []
-            blob_ind = class_blobs == u
-
-            locs = np.where(blob_ind * (points_mask))
-
-            for j in range(locs[0].shape[0]):
-                pointsList += [{"y": locs[0][j], "x":locs[1][j]}]
-
-            assert len(pointsList) == blob_counts[i]
-
-            if blob_counts[i] == 1:
-                n_single += 1
-
-            else:
-                n_multi += 1
-            size = blob_ind.sum()
-            total_size += size
-            blobList += [{"class": l, "size": size,
-                          "label": u, "n_points": blob_counts[i],
-                          "pointsList":pointsList}]
-
-    blob_dict = {"blobs": blobs, "blobList": blobList,
-                 "n_fp": n_fp,
-                 "n_single": n_single,
-                 "n_multi": n_multi,
-                 "total_size": total_size}
-
-    return blob_dict
 
 
 def blobs2points(blobs):
@@ -278,12 +183,58 @@ def compute_game(pred_points, gt_points, L=1):
             se += float(abs(gt_count.sum() - pred_count.sum()))
     return se
 
-def save_tmp(fname, images, logits, points):
+def save_tmp(fname, images, logits, radius, points):
     from haven import haven_utils as hu
     probs = F.softmax(logits, 1); 
-    mask = probs.argmax(dim=1).cpu().numpy().astype('uint8').squeeze()*255
+    mask = probs.argmax(dim=1).cpu().numpy().astype('uint8').squeeze()
     img_mask=hu.save_image('tmp2.png', 
                 hu.denormalize(images, mode='rgb'), 
                 mask=mask, return_image=True)
-    hu.save_image(fname,np.array(img_mask)/255. , 
-                    points=att_dict['points'])
+    hu.save_image(fname,np.array(img_mask)/255. , radius=radius,
+                    points=points)
+
+def get_random_points(mask, n_points=1, seed=1):
+    from haven import haven_utils as hu
+    y_list, x_list = np.where(mask)
+    points = np.zeros(mask.squeeze().shape)
+    with hu.random_seed(seed):
+        for i in range(n_points):
+            yi = np.random.choice(y_list)
+            x_tmp = x_list[y_list == yi]
+            xi = np.random.choice(x_tmp)
+            points[yi, xi] = 1
+
+    return points
+
+def get_points_from_mask(mask, bg_points=0):
+    n_points = 0
+    points = np.zeros(mask.shape)
+    # print(np.unique(mask))
+    assert(len(np.setdiff1d(np.unique(mask),[0,1,2] ))==0)
+
+    for c in np.unique(mask):
+        if c == 0:
+            continue
+        blobs =  morph.label((mask==c).squeeze())
+        points_class = blobs2points(blobs)
+
+        ind = points_class!=0
+        n_points += int(points_class[ind].sum())
+        points[ind] = c
+    assert morph.label((mask).squeeze()).max() == n_points
+    points[points==0] = 255
+    if bg_points == -1:
+       bg_points = n_points
+
+    if bg_points:
+        from haven import haven_utils as hu
+        y_list, x_list = np.where(mask==0)
+        with hu.random_seed(1):
+            for i in range(bg_points):
+                yi = np.random.choice(y_list)
+                x_tmp = x_list[y_list == yi]
+                xi = np.random.choice(x_tmp)
+                points[yi, xi] = 0
+
+    return points
+        
